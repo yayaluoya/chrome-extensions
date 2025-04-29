b3a8f974a2a066ffbe84fd73cc6e899a68d78f3e
YAYALUOYA <yayaluoya@sina.com>
2025-04-29 22:06:34 +0800

    feat: 优化消息的提示

--- packages/background/src/index.ts
+++ packages/background/src/index.ts
@@ -1,5 +1,6 @@
 import { AllUrlsMatches } from "./constant/const";
 import { startServer } from "./startServer";
+import { tapdLocalStorage } from "@taozi-chrome-extensions/common/src/local/tapd";
 
 chrome.scripting.registerContentScripts([
   {
@@ -10,5 +11,25 @@ chrome.scripting.registerContentScripts([
     runAt: "document_idle"
   }
 ]);
+
+setInterval(async () => {
+  const v = await tapdLocalStorage.get();
+  const { bug = 0 } = v?.workitemCount || {};
+  if (bug > 0) {
+    chrome.action.setIcon({
+      path: {
+        "64": "/images/icon-msg-64.png",
+        "128": "/images/icon-msg-128.png"
+      }
+    });
+  } else {
+    chrome.action.setIcon({
+      path: {
+        "64": "/images/icon-64.png",
+        "128": "/images/icon-128.png"
+      }
+    });
+  }
+}, 100);
 
 startServer();
