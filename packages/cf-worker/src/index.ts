16d463c52e81ed77786e58291fda7697725b970e
YAYALUOYA <yayaluoya@sina.com>
2025-04-30 14:40:00 +0800

    feat: 添加cf的workder用来执行爬虫任务

---
+++ packages/cf-worker/src/index.ts
@@ -0,0 +1,27 @@
+export default {
+	/**
+	 * This is the standard fetch handler for a Cloudflare Worker
+	 *
+	 * @param request - The request submitted to the Worker from the client
+	 * @param env - The interface to reference bindings declared in wrangler.jsonc
+	 * @param ctx - The execution context of the Worker
+	 * @returns The response to be sent back to the client
+	 */
+	async fetch(request, env, ctx): Promise<Response> {
+		const url = new URL(request.url);
+		const target = request.headers.get('x-target') || '';
+		if (!target) {
+			return new Response('请求头中缺少 x-target', {
+				status: 500,
+			});
+		}
+		return fetch(new URL(`${url.pathname}${url.search}`, target), {
+			method: request.method,
+			body: request.body,
+			headers: [...request.headers, ['referer', target]].reduce<Record<string, string>>((a, [key, value]) => {
+				a[key] = value;
+				return a;
+			}, {}),
+		});
+	},
+} satisfies ExportedHandler<Env>;
